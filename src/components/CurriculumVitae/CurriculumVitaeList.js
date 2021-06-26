f2454ea86af876d49462581f91d067daadec0c9a
Sadikortaoglan <sadik.ortaoglan@gmail.com>
2021-06-26 18:41:54 +0300

    added component

--- src/components/CurriculumVitae/CurriculumVitaeList.js
+++ src/components/CurriculumVitae/CurriculumVitaeList.js
@@ -1,44 +1,63 @@
 import React from "react";
 import CurriculumVitaeService from "../../services/curriculumVitaeService";
 import { useState, useEffect } from "react";
-import { Table, Container } from "semantic-ui-react";
+import { useParams } from "react-router-dom";
+import { Card,Image,Button } from "semantic-ui-react";
 
-export default function CurriculumVitaeList() {
+export default function CurriculumVitaeList() { 
+  let { id } = useParams();
+  const [auths, setauth] = useState([]);
+  const [languages, setlanguages] = useState([]);
+  const [cities, setcities] = useState([]);
+  const [capabilities, setcapabilities] = useState([]);
+  const [socials, setsocial] = useState([]);
+  const [workExperiences, setworkExperiences] = useState([]);
   const [curriculumVitaes, setcurriculumVitaes] = useState([]);
+  const [jobSeekers, setjobSeekers] = useState([]);
+  const [schools, setschools] = useState([])
+
+ 
   useEffect(() => {
     let curriculumVitaeService = new CurriculumVitaeService();
-    curriculumVitaeService
-      .getAll().then((result) => setcurriculumVitaes(result.data.data));
-  });
+    curriculumVitaeService.findByJobSeekerId(id).then((result) => {
+      setcurriculumVitaes(result.data.data)
+      setlanguages(result.data.languages)
+      setauth(result.data.data.auths)
+      setcities(result.data.data.cities)
+      setcapabilities(result.data.data.capabilities)
+      setsocial(result.data.data.socials)
+      setworkExperiences(result.data.data.workExperiences)
+      setjobSeekers(result.data.data.jobSeekers)
+      setschools(result.data.data.school);
+      console.log(result)
+    });
+  },[id]);
+
   return (
     <div>
-      <div>
-        <Container>
-          <Table fixed>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>Ad</Table.HeaderCell>
-                <Table.HeaderCell>Soyad</Table.HeaderCell>
-                <Table.HeaderCell>Deneyim</Table.HeaderCell>
-                <Table.HeaderCell>Okul Adı</Table.HeaderCell>
-                <Table.HeaderCell>Dil</Table.HeaderCell>
-                <Table.HeaderCell>Yetenekler</Table.HeaderCell>
-                <Table.HeaderCell>Sosyal Medya</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {curriculumVitaes.map((curriculumVitae) => (
-                <Table.Row key={curriculumVitae.id}>
-                  <Table.Cell>{curriculumVitae.jobSeeker.firstName}</Table.Cell>
-                  <Table.Cell>{curriculumVitae.jobSeeker.lastName}</Table.Cell>
-                  <Table.Cell>{curriculumVitae.capabilities.name}</Table.Cell>
-                  <Table.Cell>{curriculumVitae.language.language}</Table.Cell>
-                </Table.Row>
-              ))}
-            </Table.Body>
-          </Table>
-        </Container>
-      </div>
+      <Card.Group>
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated='right'
+          size='mini'
+          src='/images/avatar/large/steve.jpg'
+        />
+        <Card.Header>Steve Sanders</Card.Header>
+        <Card.Meta>Friends of Elliot</Card.Meta>
+        <Card.Description>
+          Steve wants to add you to the group <strong>best friends</strong>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div className='ui two buttons'>
+          <Button basic color='red'>
+            Decline
+          </Button>
+        </div>
+      </Card.Content>
+    </Card>   
+  </Card.Group>
     </div>
   );
 }
---
+++ src/components/JobPosting/JobPostingAdd.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function JobPostingAdd() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
